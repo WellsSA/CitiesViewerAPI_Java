2cb4c0c1db4f7fecfdcc8a13710b03bdda9531a5
WellsSA <wel.cavzod@gmail.com>
2020-05-31 20:30:39 -0300

    chore(): base project definition

---
+++ src/main/java/br/usjt/usjt_ccp3anmca_rest_json_cities/UsjtCcp3anmcaRestJsonCitiesApplication.java
@@ -0,0 +1,13 @@
+package br.usjt.usjt_ccp3anmca_rest_json_cities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsjtCcp3anmcaRestJsonCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsjtCcp3anmcaRestJsonCitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/usjt/usjt_ccp3anmca_rest_json_cities/UsjtCcp3anmcaRestJsonCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usjt.usjt_ccp3anmca_rest_json_cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsjtCcp3anmcaRestJsonCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
