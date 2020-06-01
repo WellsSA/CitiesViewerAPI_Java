65ed9e6e002453b2a634b06ad594fa6c88ad4698
WellsSA <wel.cavzod@gmail.com>
2020-05-31 21:03:35 -0300

    feature(): applying @query and find by latitude and longitude

--- src/main/java/br/usjt/usjt_ccp3anmca_rest_json_cities/model/repository/CityRepository.java
+++ src/main/java/br/usjt/usjt_ccp3anmca_rest_json_cities/model/repository/CityRepository.java
@@ -3,6 +3,7 @@ package br.usjt.usjt_ccp3anmca_rest_json_cities.model.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.usjt.usjt_ccp3anmca_rest_json_cities.model.beans.City;
@@ -11,5 +12,10 @@ import br.usjt.usjt_ccp3anmca_rest_json_cities.model.beans.City;
 public interface CityRepository extends JpaRepository<City, Long>{
 	
 	public List<City> findAllByNameIsStartingWith(String letter);
+	
+	
+	@Query("SELECT entity FROM City entity WHERE entity.latitude = :latitude AND entity.longitude = :longitude")
+    public City findLatitudeLongitude(Double latitude, Double longitude);
+	// ou: public City findByLatitudeAndLongitude(Double latitude, Double longitude);
 	
 }
--- src/main/java/br/usjt/usjt_ccp3anmca_rest_json_cities/model/resources/CityResource.java
+++ src/main/java/br/usjt/usjt_ccp3anmca_rest_json_cities/model/resources/CityResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -54,5 +55,11 @@ public class CityResource {
 	@GetMapping ("list/firstLetter/{letter}")
 	public List<City> findByNameFirstLetter (@PathVariable String letter) {
 		return cityRepo.findAllByNameIsStartingWith(letter);
+	}
+	
+	@GetMapping ("find")
+	public City findLatitudeLongitude (@RequestParam("latitude") Double latitude, @RequestParam("longitude") Double longitude) {
+		System.out.println(latitude + " aaaaaaaaa " + longitude);
+		return cityRepo.findLatitudeLongitude(latitude, longitude);
 	}
 }
